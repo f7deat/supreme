77ca59374c8f0701c93f5b076f64b0e4eeec917d
f7deat <tandc.it4@vnpt.vn>
2022-04-06 17:34:37 +0700

    interceptors

--- src/app.tsx
+++ src/app.tsx
@@ -28,7 +28,6 @@ export async function getInitialState(): Promise<{
   const fetchUserInfo = async () => {
     try {
       const msg = await queryCurrentUser();
-      debugger;
       return msg;
     } catch (error) {
       history.push(loginPath);
@@ -72,15 +71,15 @@ export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) =
     },
     links: isDev
       ? [
-          <Link to="/umi/plugin/openapi" target="_blank">
-            <LinkOutlined />
-            <span>OpenAPI</span>
-          </Link>,
-          <Link to="/~docs">
-            <BookOutlined />
-            <span>Document</span>
-          </Link>,
-        ]
+        <Link to="/umi/plugin/openapi" target="_blank">
+          <LinkOutlined />
+          <span>OpenAPI</span>
+        </Link>,
+        <Link to="/~docs">
+          <BookOutlined />
+          <span>Document</span>
+        </Link>,
+      ]
       : [],
     menuHeaderRender: undefined,
     // 自定义 403 页面
--- src/services/config.ts
+++ src/services/config.ts
@@ -2,13 +2,22 @@ import { extend } from 'umi-request';
 
 export const DOMAIN = 'https://defzone.net';
 
-const token = () => localStorage.getItem('def_token');
-
 const request = extend({
-  prefix: `https://defzone.net/api`,
-  headers: {
-    authorization: `Bearer ${token()}`,
-  },
+  prefix: `https://defzone.net/api`
 });
+
+request.interceptors.request.use(async (url, options) => {
+
+  const token = localStorage.getItem("def_token");
+  const headers = {
+    'authorization': `Bearer ${token}`
+  };
+  return (
+    {
+      url: url,
+      options: { ...options, headers: headers },
+    }
+  );
+})
 
 export default request;
